f6e7ba2647cf361b9f2e19d8cbe6c783dbcbf0bf
Andrei Istoc <andreistoc@yahoo.com>
2018-09-19 21:23:15 +0300

    Added extra workout elements and variables

--- Ladder App/Controller/WorkoutController.swift
+++ Ladder App/Controller/WorkoutController.swift
@@ -13,6 +13,16 @@ class WorkoutController: UIViewController {
     let restColor = #colorLiteral(red: 0, green: 0.6549019608, blue: 0.8823529412, alpha: 1)
     let exerciseColor = #colorLiteral(red: 1, green: 0.368627451, blue: 0.3568627451, alpha: 1)
     
+    var isAscending: Bool = true
+    var isAntagonist: Bool = true
+    var isWaving: Bool = true
+    var maximumReps: Int = 10
+    var timePerRep: Int = 5
+    var restNeeded: Int = 30
+    var setsDone: Int = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
