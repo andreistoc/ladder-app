846a62f0e1cbb1cbfd05b503602606e0930904e7
Andrei Istoc <andreistoc@yahoo.com>
2018-10-04 20:21:46 +0300

    Added settings info screen

--- Ladder App/Controller/LearnController.swift
+++ Ladder App/Controller/LearnController.swift
@@ -16,15 +16,15 @@ class LearnController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        // Sets the status bar to hidden when the view has finished appearing
+        let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView
+        statusBar.isHidden = true
     }
-    */
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // Sets the status bar to visible when the view is about to disappear
+        let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView
+        statusBar.isHidden = false
+    }
 }
